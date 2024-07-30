d993be732542ebdfc27fe85ca964e441b0e351fc
Moddy-t <amalembamodesta@gmail.com>
2024-07-30 12:05:44 +0300

    running tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Article() {
+function Article(props) {
   return (
     <div>
-      {/* please pass this test */}
+       please pass this test 
     </div>
   );
 }
 
 export default Article;
+
+
+
+
